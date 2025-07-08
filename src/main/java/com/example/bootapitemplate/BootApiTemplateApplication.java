96c130d02e9bdbb12a5d0bf5cd8c5b3f05bd31f1
jina <imbolic0301@gmail.com>
2025-07-08 12:15:30 +0900

    Initial Commit

---
+++ src/main/java/com/example/bootapitemplate/BootApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootapitemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApiTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApiTemplateApplication.class, args);
+    }
+
+}
